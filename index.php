1a248aee1e7ac812fa18fc936e15ad7941ad20a0
lillian rose winter <vulpes@vore.city>
2020-06-03 08:00:29 +0000

    change name from fen to neko

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<p>hi! welcome to tilde.pw.</p>
 	<p>tilde.pw is a unix server with some people on it and that unix server hosts webpages and other things too.</p>
 	<p>it is <a href="https://tildeverse.org">one of many</a> servers attempting to create a more <i>empathetic</i> and <i>open</i> programming community.</p>
-	<p>tilde.pw's sysadmins are <a href="~fen/">~fen</a> and <a href="~anton/">~anton</a>.</p>
+	<p>tilde.pw's sysadmins are <a href="~neko/">~neko</a> and <a href="~anton/">~anton</a>.</p>
 	<br><hr>
 	<p>here are our ssh host keys for your verification and security:</p>
 	<p>ECDSA: SHA256:l9U2XAtdy0JDSgH1ZqhfwQHSZLUM0AQfJDotCy/67zM</p>
